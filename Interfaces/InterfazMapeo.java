938058730a156453872b11b868b35b05593a67a3
Melylennon95 <melylennon@gmail.com>
2015-11-23 17:24:06 -0600

    update interfaz de mapeo

--- Interfaces/InterfazMapeo.java
+++ Interfaces/InterfazMapeo.java
@@ -24,10 +24,21 @@ import controles.ControlAdministrador;
     out.println("<HTML>");
     out.println("<HEAD>");
     out.println("<META http-equiv=Content-Type content=\"text/html\">");
+    out.println("<link rel=stylesheet href=css/bootstrap.min.css>");
+    out.println("<link rel=stylesheet href=style.css>");
     out.println("</HEAD>");
     out.println("<BODY>");
     out.println("<TITLE>Vinculacion Academica</TITLE>");
     out.println("<h2>Mapeo de proyectos</h2>");
+    out.println("<address>");
+    out.println("<strong>Twitter, Inc.</strong><br>");
+    out.println("1355 Market Street, Suite 900<br>");
+    out.println("San Francisco, CA 94103<br>");
+    out.println("<abbr title=Phone>P:</abbr> (123) 456-7890");
+    out.println("</address>");
+    out.println("<img src=sampleMap.JPG alt=... class=img-rounded>");
+    out.println("<form method=\"GET\" action=\"index.html\">");
+    out.println("<button class=\"btn btn-danger \"type=\"submit\">Terminar</button>");
   }
 
 }
